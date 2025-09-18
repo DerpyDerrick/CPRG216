1a9d4e14468991f5165ab8fb895a4b7e763c874f
DerpyDerrick <derickhesait@gmail.com>
2025-09-18 15:43:03 -0600

    added files

---
+++ week3.py
@@ -0,0 +1,18 @@
+# This is our 3rd class demo
+'''
+In this class we will work on variables, 
+operators and other stuff
+'''
+# Review
+# assignment
+i = 7 # int
+f = 6.9 # float
+text = "Hello" # String
+condition = True # Bool
+
+
+
+print("The value of variable i is:" ,i," and the type is ",type(i))
+print("The value of variable f is:" ,f," and the type is ",type(f)) 
+print("The value of variable text is:" ,text," and the type is ",type(text)) 
+print("The value of variable condition is:" ,condition," and the type is ",type(condition))
