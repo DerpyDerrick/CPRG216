d4fcf94b28cfbff026f29ce1a71edb8c211a6612
DerpyDerrick <derickhesait@gmail.com>
2025-09-18 16:06:00 -0600

    added circle.py and updated week3.py

--- week3.py
+++ week3.py
@@ -10,9 +10,23 @@ f = 6.9 # float
 text = "Hello" # String
 condition = True # Bool
 
-
-
 print("The value of variable i is:" ,i," and the type is ",type(i))
 print("The value of variable f is:" ,f," and the type is ",type(f)) 
 print("The value of variable text is:" ,text," and the type is ",type(text)) 
 print("The value of variable condition is:" ,condition," and the type is ",type(condition))
+# some function call : print, input, int, float, str, bool
+num_as_text = "43"
+
+num_as_num = int(num_as_text) # converting string (text) to num
+
+print(num_as_text) # will print as a text
+print(num_as_num) # will it print???
+print(str(num_as_num)) #equivalent
+
+num = 3
+num_f = float(3)
+
+num2 = 3.4
+num2_i = int(num2)
+
+num2_as_text = str(num2)
